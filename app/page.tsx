e2dc06e08691f287de6350faa0ab4a896f8a9a0a
Juan Bernal <mesieou@gmail.com>
2025-08-07 21:13:35 +1000

    updated app to next.js 15.4 and react 19

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
-import { getHumeAccessToken } from "@/features/web-voice-assistant/utils/getHumeAccessToken";
-import dynamic from "next/dynamic";
-
-const Chat = dynamic(() => import("@/features/web-voice-assistant/components/Chat"), {
-  ssr: false,
-});
 
 export default async function Page() {
-  const accessToken = await getHumeAccessToken();
-
-  if (!accessToken) {
-    throw new Error('Unable to get access token');
-  }
-
   return (
     <div className={"grow flex flex-col"}>
-      <Chat accessToken={accessToken} />
-    </div>
+      <h1>Welcome to Skedy</h1>
+      </div>
   );
 }
